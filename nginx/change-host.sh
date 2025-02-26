c8da33e6c3032ab36dd6781d2a057e2cff2b500a
Ricardo Paes <ricardo@likesistemas.com.br>
2025-02-25 23:54:02 -0300

    🔧 Setting up reverse proxy for bore server

--- nginx/change-host.sh
+++ nginx/change-host.sh
@@ -2,4 +2,5 @@
 
 echo "Changing host to ${BORE_HOST}"
 dockerize -template /etc/nginx/conf.d/default.tpl:/etc/nginx/conf.d/default.conf \
+          -template /etc/nginx/bore-server.tpl:/etc/nginx/bore-server.conf \
           -wait tcp://$BORE_HOST:7835 -timeout 60s
